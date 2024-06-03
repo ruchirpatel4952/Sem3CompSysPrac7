d388124446a293c1bdef3d45b93185ce76193dbc
Ruchir <ruchirpatel2000@gmail.com>
2024-06-03 20:37:58 +0930

    first commit-- first edit

--- Token.py
+++ Token.py
@@ -3,5 +3,11 @@ class Token:
         self.type = type
         self.value = value
 
-    def __repr__(self):
-        return f"Token(type={self.type}, value={self.value})"
+    def getType(self):
+        return self.type
+
+    def getValue(self):
+        return self.value
+
+    def __str__(self):
+        return f'Token(type={self.type}, value={self.value})'
